3e3865006bee97677f5e0a1aba42f5343e6609cb
Alexandra Gieger <giegeralexandra@gmail.com>
2020-07-28 22:38:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(string)
   elsif string == string.upcase
     print "NO, NOT SINCE 1938!"
   else 
-    puts "HUH?! SPEAK UP, SONNY!"
+    print "HUH?! SPEAK UP, SONNY!"
   end
 end
     
