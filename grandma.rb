d27e942fbd67bc009fec3da8e5b4ab332af56139
Alexandra Gieger <giegeralexandra@gmail.com>
2020-07-28 22:38:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    print "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
-    puts "NO, NOT SINCE 1938!"
+    print "NO, NOT SINCE 1938!"
   else 
     puts "HUH?! SPEAK UP, SONNY!"
   end
